e8d0376d93b32f43723400c2d179fdb13bb21577
Ella Fox <tinyfluffs@tutanota.com>
2022-02-22 12:14:29 +0100

    New post formatting

--- lib/main_view.dart
+++ lib/main_view.dart
@@ -1,5 +1,5 @@
 import 'package:app/drawer.dart';
-import 'package:app/main_pages.dart';
+import 'package:app/post_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -19,13 +19,13 @@ class ReactiveView extends StatelessWidget {
             child: mainDrawer(),
           ),
           Container(width: 0.5, color: Colors.black),
-          Expanded(child: PageView(children: mainPages(context)))
+          Expanded(child: mainView(context))
         ],
       );
     } else {
       return Scaffold(
         appBar: AppBar(
-          title: const Text("Placeholder"),
+          title: const Text("Feed"),
           leading: Builder(
             builder: (BuildContext ctx) {
               return IconButton(
@@ -39,8 +39,43 @@ class ReactiveView extends StatelessWidget {
           ),
         ),
         drawer: mainDrawer(),
-        body: PageView(children: mainPages(context)),
+        body: mainView(context),
       );
     }
   }
+}
+
+Widget mainView(BuildContext context) {
+  return Scaffold(
+    body: const Center(child: CircularProgressIndicator()),
+    floatingActionButton: FloatingActionButton.extended(
+      label: const Text("New Post"),
+      icon: const Icon(Icons.add_sharp),
+      onPressed: () {
+        Navigator.push(context, _postRouteBuilder(const PostView()));
+      },
+    ),
+  );
+}
+
+Route _postRouteBuilder(Widget view) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return view;
+    },
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return SlideTransition(
+        position: animation.drive(
+          Tween(
+            begin: const Offset(0.0, 1.0),
+            end: Offset.zero,
+          ).chain(
+            CurveTween(curve: Curves.easeInOut),
+          ),
+        ),
+        child: child,
+      );
+    },
+    transitionDuration: const Duration(milliseconds: 200),
+  );
 }
--- lib/post_view.dart
+++ lib/post_view.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+double marginWidth(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  if (width > 1900.0) return (width / 100) * (100 / 3);
+  if (width > 1280.0) return (width / 100) * 25;
+  if (width > 900.0) return (width / 100) * 20;
+  if (width > 600.0) return (width / 100) * 10;
+  if (width > 300.0) return 8.0;
+  return 8.0;
+}
+
 class PostView extends StatelessWidget {
   const PostView({Key? key}) : super(key: key);
 
@@ -20,7 +30,45 @@ class PostView extends StatelessWidget {
           },
         ),
       ),
-      body: const Center(child: CircularProgressIndicator()),
+      body: Container(
+        margin:
+            EdgeInsets.symmetric(horizontal: marginWidth(context), vertical: 8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.all(8),
+              child: TextField(
+                minLines: 2,
+                maxLines: 8,
+                textAlignVertical: TextAlignVertical.top,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(),
+                  hintText: "What's on your mind?",
+                ),
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(16),
+                ),
+                onPressed: () {},
+                child: const Icon(
+                  Icons.send_sharp,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
